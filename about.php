3f43682995bfe1046b290bd3abc3fd7ad4b5210c
Rajarsi Saha <rajarsi3997@gmail.com>
2020-11-23 16:01:55 +0530

    Day 11 updates

--- about.php
+++ about.php
@@ -84,7 +84,7 @@ License URL: https://github.com/RajarsiGit/Grocery-Website/blob/main/LICENSE/
 										require_once "php/db_controller.php";
 										$db_handle = new DBController();
 										$query = "SELECT c_name from customer WHERE c_id = ".intval($_COOKIE['u_id']).";";
-      									$result =  $db_handle->run($query);  
+      									$result =  $db_handle->fetch($query);  
 										echo '<li><a href="/profile">'.explode(' ', trim($result[0]['c_name']))[0].'</a></li><li><a href="" onclick="$.removeCookie(\'u_id\') = \'\'; location.reload();">Logout</a></li>';
 									}else{
 										echo '<li><a href="/login">Login</a></li>';
