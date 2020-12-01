ba970360c6eeedc51f781c6a8b39e268a5647e68
Rajarsi Saha <rajarsi3997@gmail.com>
2020-12-01 23:26:17 +0530

    Day 16 updates

--- php/mail.php
+++ php/mail.php
@@ -1,47 +1,29 @@
 <?php
-    require_once "PHPMailer/PHPMailerAutoload.php";
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST)) {
+        $name = $_POST["Name"];
+        $email = $_POST["Email"];
+        $phone = $_POST["Telephone"];
+        $subject = $_POST["Subject"];
+        $text= $_POST["Message"];
 
-    $name = $_POST["Name"];
-    $email = $_POST["Email"];
-    $phone = $_POST["Telephone"];
-    $subject = $_POST["Subject"];
-    $text= $_POST["Message"];
+        $headers = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'From: query@our-grocery.tk\r\n';
+        $headers .= 'Reply-To: ' . $email;
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    $mail = new PHPMailer;
-      
-    $mail->SMTPDebug = 3; 
-    $mail->isSMTP();                              
-    $mail->Host = "smtp-pulse.com";
-    $mail->SMTPAuth = true;                           
-    $mail->Username = "ultradevsofficial2020@gmail.com";                 
-    $mail->Password = "B3Q8ZWPnqrb";                           
-    $mail->SMTPSecure = "tls";                           
-    $mail->Port = 587;
-    $mail->setFrom('contact@our-grocery.tk', 'Our Grocery');
-    $mail->addAddress('ultradevsofficial2020@gmail.com', 'Ultra Devs');
-    $mail->addReplyTo($email, $name);
-    $mail->isHTML(true);
-    $mail->Subject = "Email from Contact Page";
-    $mail->addCustomHeader("X-SMTPProvider-MailClass: transactional");
+        $message = '<table style="width:100%">
+                <tr><td>Name: '.$name.'</td></tr>
+                <tr><td>Email: '.$email.'</td></tr>
+                <tr><td>Phone: '.$phone.'</td></tr>
+                <tr><td>Subject: '.$subject.'</td></tr>
+                <tr><td>Text: '.$text.'</td></tr>
+            </table>';
 
-    $mail->Body = '<table style="width:100%">
-        <tr><td>Name: '.$name.'</td></tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>Phone: '.$phone.'</td></tr>
-        <tr><td>Subject: '.$subject.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
-    </table>';
-
-    $mail->ALtBody = 'Name: '.$name.
-        '\nEmail: '.$email.
-        '\nPhone: '.$phone.
-        '\nSubject: '.$subject.
-        '\nText: '.$text;
-
-    if($mail->send()) {
-        echo "<div style='margin: 1em 1em 1em 1em; text-align: center; color: #85c639;'><h4>The message has been sent!</h4></div>";
-    }
-    else {
-        echo "<div style='margin: 1em 1em 1em 1em; text-align: center; color: #fa1818;'><h4>Message send failed!</h4></div>";
+        if(@mail('contact@our-grocery.tk', 'Email from Contact Page', $message, $headers)) {
+            echo "<div style='margin: 1em 1em 1em 1em; text-align: center; color: #85c639;'><h4>The message has been sent!</h4></div>";
+        }
+        else {
+            echo "<div style='margin: 1em 1em 1em 1em; text-align: center; color: #fa1818;'><h4>Message send failed!</h4></div>";
+        }
     }
 ?>
